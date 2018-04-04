1f8aed3e3164b7cb0bf4721bc740cf14fc87e293
AppsDaniel <apps.DanielLarsson@gmail.com>
2018-04-04 11:51:43 +0200

    send price to db

--- www/js/app.js
+++ www/js/app.js
@@ -56,26 +56,69 @@ $stateProvider
 .controller("apiCtlr",function($scope, $http, $state, $rootScope){
 
 
+  
+  $scope.data = {};
+  
+  $scope.submit = function(){
+    
+    var url = "http://appdaniel.com/hotel/booking/index.php";
+    
+    $http.post(url, $scope.data)
+    .then(function(response){
+      $scope.response = response;
+      console.log($scope.response);
+    })
+    
+  };
+  
+  
   $http({
     method: "GET", 
     url: "http://appdaniel.com/hotel/",
     
     params: {}
-  
+    
   }).then(function mySuccess(response) {
-      // a string, or an object, carrying the response from the server.
-      $scope.rooms = response.data;
-      $scope.statuscheck = response.status;
-  
-     // Denna används för att kolla upp vilket rum  man trycker på, nästa steg
+    // a string, or an object, carrying the response from the server.
+    $scope.rooms = response.data;
+    $scope.statuscheck = response.status;
+    
+    // Denna används för att kolla upp vilket rum  man trycker på, nästa steg
     // $scope.aRoom = $scope.rooms.find(function(item){
-    //    console.log(item.id)
-       $scope.roomToShow = $state.params.sendID;
+      //    console.log(item.id)
+      $scope.roomToShow = $state.params.sendID;
       //  return item.id == $stateParams.ID; 
       //})
-  
+      
     }, function myError(response) {
       $scope.rooms = response.statusText;
-  });
+    });
+    
+    
+    
+    $scope.nights = function(arravial, depature){
+      var arravial = new Date(arravial);
+      var depature = new Date(depature);
+      var countdays = Math.abs(depature.getTime() - arravial.getTime());
+      
+      $scope.dayDiff = Math.ceil(countdays / (1000 * 3600 * 24));
+      if($scope.dayDiff == 0){
+        $scope.dayDiff = 1;
+        
+      }
+      return $scope.dayDiff;
+      
+    }
+    
+    //Räkna ut totalpris
+    $scope.totalPrice = function(price, nights){
+      var price;
+      var nights;
+      $scope.priceToReturn = price * nights;
+      
+      return $scope.priceToReturn;
+    }
+
   
+    
   })
